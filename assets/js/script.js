53f81a5791cf9afc4a13e5a64c84ee97de143d86
Edson Augusto Schmidt <dinho.a.schmidt@hotmail.com>
2023-07-06 15:44:12 -0300

    up relogio

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const horas= document.getElementById("horas");
+const minutos= document.getElementById("minutos");
+const segundos= document.getElementById("segundos");
+const dia= document.getElementById("dia");
+const mes= document.getElementById("mes");
+const ano= document.getElementById("ano");
+
+const relogio= setInterval(function time(){
+    let datetoday = new Date()
+    let hr= datetoday.getHours()
+    let min= datetoday.getMinutes()
+    let s= datetoday.getSeconds()
+    let d= datetoday.getDate()
+    let m=datetoday.getMonth() +1
+    let a= datetoday.getFullYear()
+
+
+    if (hr<10) hr= '0' + hr;
+    if (min<10) min= '0' + min;
+    if (s<10) s= '0' + s;
+    if (d<10) d= '0' + d;
+    if (m<10) m= '0' + m;
+    
+
+
+    
+    horas.textContent=hr;
+    minutos.textContent=min;
+    segundos.textContent=s;
+    dia.textContent=d;
+    mes.textContent=m;
+    ano.textContent=a;
+
+})
